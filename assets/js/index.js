74ddf0d76727d17ecbf61e5e74d5256a8bba3d9a
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2022-07-15 18:07:32 -0600

    another commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,13 +4,11 @@ function myFunction() {
     document.getElementById("frontalLobe").innerHTML =
         "<h3> The frontal lobes are important for voluntary movement, expressive language and for managing higher level executive functions. Executive functions refer to a collection of cognitive skills including the capacity to plan, organise, initiate, self-monitor and control one's responses in order to achieve a goal. ...</h3>";
 }
-
 document.getElementById("parietalLobe").addEventListener("mouseover", mySixthFunction);
 function mySixthFunction() {
     document.getElementById("parietalLobe").innerHTML
         = "<h3>The parietal lobes are responsible for processing somatosensory information from the body; this includes touch, pain, temperature, and the sense of limb position. Like the temporal lobes, the parietal lobes are also involved in integrating information from different modalities.... <h3>"
 }
-
 document.getElementById("temporalLobe").addEventListener("mouseover", myFifthFunction);
 function myFifthFunction() {
     document.getElementById("temporalLobe").innerHTML
@@ -35,7 +33,7 @@ function myThirdFunction() {
         = "<h3 id='afterMouseOver'>The spinal cord is a long, tube-like band of tissue. It connects your brain to your lower back. Your spinal cord carries nerve signals from your brain to your body and vice versa. These nerve signals help you feel sensations and move your body. .... <h3>"
 }
 
-
+//This is the canvas code.
 var myName = "The Brain ";
 
 var red = [0, 100, 63];
@@ -53,5 +51,18 @@ if (10 < 3) {
 else {
     bubbleShape = 'circle';
 }
-
 bounceBubbles();
+
+//This code is the stuff that changes the background color.
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function changeColor() {
+    let newColor = getRandomColor();
+    document.body.style.backgroundColor = newColor;
+}
